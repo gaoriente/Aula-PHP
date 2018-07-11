3de99506dd9b29a49391f8be4be44b36c68181c3
Gabriel Oriente <gabriel.oriente@mirumagency.com>
2018-07-11 11:00:19 -0300

    Initial commit

---
+++ HelloWorld.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8"/>
+        <title>Primeira vez</title>
+        <style>
+            h1 {
+                color: aqua;
+                text-shadow: 1px 1px 1px black;
+            }
+        </style>
+    </head>
+    <body>
+
+        <h1>Testando</h1>
+        <?php
+            $idade = 3;
+            echo $idade;
+        ?>
+
+    </body>
+</html>
